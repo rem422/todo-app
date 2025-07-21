b9046af44371e8487b4eb4502b6556e911fc3245
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-07-22 00:37:16 +0300

    Todo App

--- backend/routes/todo.route.js
+++ backend/routes/todo.route.js
@@ -1,2 +1,7 @@
 import express from 'express';
 import { Todo } from '../models/todo.model.js';
+
+const router = express.Router();
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import todoRoutes  from './routes/todo.route.js'
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,7 @@ app.get("/", (req, res) => {
     res.status(200).send("Server is running...")
 });
 
+app.use('/api/todos', todoRoutes);
 
 app.listen(PORT,() => {
     console.log('Server started at: http://localhost:5000');
