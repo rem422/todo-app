5bb8a2df2741ae92877f5bfa6f6e06dffef66983
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-07-22 19:45:32 +0300

    Todo App

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,24 @@ import todoRoutes  from './routes/todo.route.js';
 import { connectDB } from './config/db.js';
 
 dotenv.config();
+
 const app = express();
+
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
 app.use('/api/todos', todoRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+}
 
 app.listen(PORT,() => {
     connectDB();
